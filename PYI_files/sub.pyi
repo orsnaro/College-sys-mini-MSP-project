67dbddaaffa7bd5e07e3b5746589bb7fee5f57c0
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-03-26 00:49:03 +0200

    # change code info commenting style from c++ stlye to doc string # proper python style

--- PYI_files/sub.pyi
+++ PYI_files/sub.pyi
@@ -1,18 +1,11 @@
-##########################################################################################
-#                                                                                        #
-#                                                                                        #
-#                                                                                        #
-#                          Programmer : Omar Rashad                                      #
-#                          Version : v0.4 Beta                                           #
-#                          Date :  97 / 11 / 2022                                        #
-#                          Code Type : sub.pyi => Mini_proj for MSP-ML Committe          #
-#                          Title : Omar's Emgineering College System                     #
-#                          Interpreter : cPython  v3.11.0 [Compiler : MSC v.1933 AMD64]  #
-#                                                                                        #
-#                                                                                        #
-#                                                                                        #
-##########################################################################################
-
+"""
+                          Programmer : Omar Rashad                                      
+                          Version : v0.4 Beta                                           
+                          Date :  29 / 11 / 2022                                        
+                          Code Type : sub.py => Mini_proj for MSP-ML Committe        
+                          Title : Omar's Emgineering College System                     
+                          Interpreter : cPython  v3.11.0 [Compiler : MSC v.1933 AMD64]  
+"""           
 from spr import *
 
    #----------------------------------------CHILD CLASSES -----------------------------------------#
